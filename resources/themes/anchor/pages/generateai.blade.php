9780273e899fb6eb4041e3542333cfd7dd41d468
infabls <infabls.com@gmail.com>
2024-11-23 19:11:14 +0700

    update generator

--- resources/themes/anchor/pages/generateai.blade.php
+++ resources/themes/anchor/pages/generateai.blade.php
@@ -13,11 +13,19 @@ $generateImage = function () {
 
     // Отправляем запрос к Replicate API
     $response = HttpFacade::withHeaders([
-        'Authorization' => 'Token ' . env('REPLICATE_API_TOKEN'),
-    ])->post('https://api.replicate.com/v1/predictions', [
-        'version' => 'stability-ai/stable-diffusion-v1-4', // Версия модели
+        'Authorization' => 'Bearer ' . env('REPLICATE_API_TOKEN'),
+        'Content-Type' => 'application/json',
+        'Prefer' => 'wait',
+    ])->post('https://api.replicate.com/v1/models/black-forest-labs/flux-redux-dev/predictions', [
         'input' => [
-            'prompt' => $this->prompt, // Используем введённый текст
+            'guidance' => 3,
+            'megapixels' => '1',
+            'num_outputs' => 1,
+            'redux_image' => 'https://replicate.delivery/pbxt/M0mdz2nXiUmhpfLswjNdEHT3IhGtclUz7Q1sCw3XiHXzUugT/0_ZjYSm_q36J4KChdn.webp',
+            'aspect_ratio' => '4:3',
+            'output_format' => 'webp',
+            'output_quality' => 80,
+            'num_inference_steps' => 28
         ],
     ]);
 
